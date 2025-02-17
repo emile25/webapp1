78ebae5441cbd35ca3cc446ef36170b3e2547ce8
emile25 <1212164@student.roc-nijmegen.nl>
2025-02-17 13:53:16 +0100

    eerste opzet project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <title>sjabloon</title>
+</head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <main>
+        <div class="div1">
+            <div class="div2">
+
+            </div>
+    <div class="div3">
+        hello
+    </div>
+    <div class="div4">
+
+    </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
